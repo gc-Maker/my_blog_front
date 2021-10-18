d5b51f217e7c604925a79ada5524d50f93868a1d
wuliangjie <wuliangjie_1996@163.com>
2021-10-19 00:05:43 +0800

    首页

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import "./App.less"
-import {Button} from "antd";
+import  styles from "./App.module.less"
+// import {Button} from "antd";
+import React from "react";
 function App() {
   return (
-    <Button>按钮 </Button>
+    <React.Fragment>
+      <header className={styles.header}>
+        <div className={styles.inner}>
+          <div className={styles.left}>
+            gc_Maker
+          </div>
+          <ul className={styles.right}>
+            <li>主页</li>
+            <li>时间线</li>
+            <li>标签</li>
+            {/* <li>abc</li> */}
+          </ul>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.writing}></div>
+        <div className={styles.personInformation}>
+          <div className={styles.personInformationInner}>
+            <div className={styles.avatar}></div>
+            <div className={styles.nickName}>逍遥客</div>
+            <div className={styles.email}>wuliangjie_1996@163.com</div>
+          </div>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+
+      </footer>
+    </React.Fragment>
   );
 }
 
